8ab8ca71f43ec0cc5e756c281f756cd17db5635c
muriloact <muriloact@hotmail.com>
2025-09-03 15:19:02 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Portifolio-de-Trabalho/', // nome do repositório do GitHub
+  base: '/Portifolio-de-Trabalho', // nome do repositório do GitHub
 })
